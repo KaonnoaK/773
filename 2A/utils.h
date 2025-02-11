02b3100ad03c174d85f2de5b1b72c0ad891d5635
Kaon <78657124+KaonnoaK@users.noreply.github.com>
2025-02-12 01:31:05 +0530

    latest

---
+++ 2A/utils.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+#define MSG_FILE "msg.txt"
+#define MAX_MSG_SIZE 500
+
+#define CHANNEL_THRESHOLD 140
+
+#ifndef UTIL_H_
+#define UTIL_H_
+
+
+#define CYCLES uint32_t
+#define ADDR_PTR uint64_t
+
+#ifdef DUPLEX_MODE
+#define CHANNEL_SENDING_INTERVAL        0x00009FFFF
+#define CHANNEL_SYNC_TIMEMASK           0x0000FFFFF
+#else
+#define CHANNEL_SENDING_INTERVAL        0x00009FFF
+#define CHANNEL_SYNC_TIMEMASK           0x0000FFFF
+#endif
+
+#define CHANNEL_SYNC_JITTER             0x0100
+
+#define MAX_BUFFER_LEN	1024
+
+
+
+struct config {
+	int interval;
+	ADDR_PTR addr;
+};
+
+CYCLES mem_access(ADDR_PTR addr);
+CYCLES rdtscp(void);
+CYCLES get_time();
+CYCLES cc_sync();
+
+double check_accuracy(char*, int);
+
+void clflush(ADDR_PTR addr);
+
+char *string_to_binary(char *s);
+
+char *conv_char(char *data, int size, char *msg);
+
+void init_config(struct config *config, int argc, char **argv);
+
+#endif
+

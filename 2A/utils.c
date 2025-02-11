821c23d711160a5a8df53b50bca5c62236e182cb
Kaon <78657124+KaonnoaK@users.noreply.github.com>
2025-02-12 01:31:41 +0530

    latest

---
+++ 2A/utils.c
@@ -0,0 +1,177 @@
+#include "utils.h"
+
+// DO NOT MODIFY THIS FUNCTION
+double check_accuracy(char* received_msg, int received_msg_size){
+    FILE *fp = fopen(MSG_FILE, "r");
+    if(fp == NULL){
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    char original_msg[MAX_MSG_SIZE];
+    int original_msg_size = 0;
+    char c;
+    while((c = fgetc(fp)) != EOF){
+        original_msg[original_msg_size++] = c;
+    }
+    fclose(fp);
+
+    int min_size = received_msg_size < original_msg_size ? received_msg_size : original_msg_size;
+
+    int error_count = (original_msg_size - min_size) * 8;
+    for(int i = 0; i < min_size; i++){
+        char xor_result = received_msg[i] ^ original_msg[i];
+        for(int j = 0; j < 8; j++){
+            if((xor_result >> j) & 1){
+                error_count++;
+            }
+        }
+    }
+
+    return 1-(double)error_count / (original_msg_size * 8);
+}
+
+
+CYCLES mem_access(ADDR_PTR addr)
+{
+    CYCLES cycles;
+
+    asm volatile("mov %1, %%r8\n\t"
+            "lfence\n\t"
+            "rdtsc\n\t"
+            "mov %%eax, %%edi\n\t"
+            "mov (%%r8), %%r8\n\t"
+            /*"lfence\n\t"*/
+            "rdtscp\n\t"
+            "sub %%edi, %%eax\n\t"
+    : "=a"(cycles) /*output*/
+    : "r"(addr)
+    : "r8", "edi");
+
+    return cycles;
+}
+
+extern inline __attribute__((always_inline))
+void clflush(ADDR_PTR addr)
+{
+    asm volatile ("clflushopt (%0)"::"r"(addr));
+}
+
+
+extern inline __attribute__((always_inline))
+CYCLES rdtscp(void) {
+	CYCLES cycles;
+	asm volatile ("rdtscp"
+	: /* outputs */ "=a" (cycles));
+
+	return cycles;
+}
+
+inline CYCLES get_time() {
+    return rdtscp();
+}
+
+extern inline __attribute__((always_inline))
+CYCLES cc_sync() {
+    while((get_time() & CHANNEL_SYNC_TIMEMASK) > CHANNEL_SYNC_JITTER) {}
+    return get_time();
+}
+
+char *string_to_binary(char *s)
+{
+    if (s == NULL) return 0; 
+
+    size_t len = strlen(s) - 1;
+
+
+    char *binary = malloc(len * 8 + 1);
+    binary[0] = '\0';
+
+    for (size_t i = 0; i < len; ++i) {
+        char ch = s[i];
+        for (int j = 7; j >= 0; --j) {
+            if (ch & (1 << j)) {
+                strcat(binary, "1");
+            } else {
+                strcat(binary, "0");
+            }
+        }
+    }
+
+    return binary;
+}
+
+char *conv_char(char *data, int size, char *msg)
+{
+    for (int i = 0; i < size; i++) {
+        char tmp[8];
+        int k = 0;
+
+        for (int j = i * 8; j < ((i + 1) * 8); j++) {
+            tmp[k++] = data[j];
+        }
+
+        char tm = strtol(tmp, 0, 2);
+        msg[i] = tm;
+    }
+
+    msg[size] = '\0';
+    return msg;
+}
+
+uint64_t get_cache_set_index(ADDR_PTR phys_addr)
+{
+    uint64_t mask = ((uint64_t) 1 << 16) - 1;
+    return (phys_addr & mask) >> 6;
+}
+
+void init_config(struct config *config, int argc, char **argv)
+{
+		int *data = (int*) malloc(64*64*64);
+
+		config->interval = CHANNEL_SENDING_INTERVAL;
+
+		int *addr = data;
+        while(get_cache_set_index((ADDR_PTR) addr)) {
+            addr += 64;
+        }
+	
+		config->addr = (ADDR_PTR) addr;
+
+		/*
+	// Parse the command line flags
+	int option;
+	while ((option = getopt(argc, argv, "i:o:f:")) != -1) {
+		switch (option) {
+			case 'i':
+				echit(1);
+			default:
+				print_help();
+				exit(1);
+		}
+	}
+	
+	*/
+}
+
+bool detect_bit(struct config *config)
+{
+		int hits = 0;
+		int misses = 0;
+
+	// Sync with sender
+	CYCLES start_t = cc_sync();
+	while ((get_time() - start_t) < config->interval) {
+		CYCLES access_time = mem_access(config->addr);
+        /*printf("%d\n", access_time);*/
+		// Check if detected 1 or 0
+		if (access_time > CHANNEL_THRESHOLD) {
+			misses++;
+		} else {
+			hits++;
+		}
+	}
+
+	return misses >= hits;
+}
+

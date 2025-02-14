d8f2481a7d380ce2de6764a26264ad6ed76559bf
Kaon <78657124+KaonnoaK@users.noreply.github.com>
2025-02-14 17:03:59 +0530

    latest

--- 2B/receiver.c
+++ 2B/receiver.c
@@ -1,37 +1,44 @@
 #include "utils.h"
 
-bool detect_bit(struct config *config);
+bool detect_bit(uint64_t *addr);
 
  int main(int argc, char **argv)
 {
-	
-	// Initialize config and local variables
-	struct config config;
-	init_configR(&config, argc, argv );
+	uint64_t *cache = (uint64_t*)malloc(CACHE_SIZE);
+
 	char msg_ch[MAX_BUFFER_LEN + 1];
+	bzero(msg_ch,MAX_BUFFER_LEN + 1);
 
 	uint32_t bitSequence = 0;
 	uint32_t sequenceMask = ((uint32_t) 1<<6) - 1;
 	uint32_t expSequence = 0b101011;
-	
+	int cnt=0;
 	printf("Listening...\n");
 
 	while (1) {
-		bool bitReceived = detect_bit(&config);
+		bool bitReceived = detect_bit(cache);
 
 		// Detect the sequence '101011' that indicates sender is sending a message	
 		bitSequence = ((uint32_t) bitSequence<<1) | bitReceived;
-		if ((bitSequence & sequenceMask) == expSequence) {
+		//if ((bitSequence & sequenceMask) == expSequence) {
+		if(1){
 			int binary_msg_len = 0;
 			int strike_zeros = 0;
 			for (int i = 0; i < MAX_BUFFER_LEN; i++) {
 				binary_msg_len++;
 
-				if (detect_bit(&config)) {
+				if (detect_bit(cache)) {
+				
 					msg_ch[i] = '1';
+					printf("\n %c \n",msg_ch[i]);
+					cnt++;
+		printf("\n \t %d \n",cnt);
 					strike_zeros = 0;
 				} else {
 					msg_ch[i] = '0';
+					printf("\n %c \n",msg_ch[i]);
+					cnt++;
+		printf("\n \t %d \n",cnt);
 					if (++strike_zeros >= 8 && i % 8 == 0) {
 						break;
 					}
@@ -46,10 +53,7 @@ bool detect_bit(struct config *config);
 			
 			printf("\n\n\n Accuracy (%%): %f\n", check_accuracy(msg_ch, ascii_msg_len)*100);
 	
-			// Terminate loop if received "exit" message
-			if (strcmp(msg, "exit") == 0) {
-				break;
-			}
+			
 		}
 	}
 

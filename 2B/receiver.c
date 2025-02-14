a6d4f083c2167e1dd86cf140fd7ce38805256bde
Kaon <78657124+KaonnoaK@users.noreply.github.com>
2025-02-15 00:30:11 +0530

    latest

--- 2B/receiver.c
+++ 2B/receiver.c
@@ -1,10 +1,12 @@
 #include "utils.h"
 
-bool detect_bit(uint64_t *addr);
+bool detect_bit(Node *addr);
 
  int main(int argc, char **argv)
 {
-	uint64_t *cache = (uint64_t*)malloc(CACHE_SIZE);
+	Node *head = NULL;
+	create_ll(&head,FILL);
+
 
 	char msg_ch[MAX_BUFFER_LEN + 1];
 	bzero(msg_ch,MAX_BUFFER_LEN + 1);
@@ -16,29 +18,31 @@ bool detect_bit(uint64_t *addr);
 	printf("Listening...\n");
 
 	while (1) {
-		bool bitReceived = detect_bit(cache);
+		
+		bool bitReceived = detect_bit(head);
 
 		// Detect the sequence '101011' that indicates sender is sending a message	
 		bitSequence = ((uint32_t) bitSequence<<1) | bitReceived;
-		//if ((bitSequence & sequenceMask) == expSequence) {
-		if(1){
+		
+		if ((bitSequence & sequenceMask) == expSequence) {
+		//if(1){
 			int binary_msg_len = 0;
 			int strike_zeros = 0;
 			for (int i = 0; i < MAX_BUFFER_LEN; i++) {
 				binary_msg_len++;
 
-				if (detect_bit(cache)) {
+				if (detect_bit(head)) {
 				
 					msg_ch[i] = '1';
-					printf("\n %c \n",msg_ch[i]);
+					//printf("\n %c \n",msg_ch[i]);
 					cnt++;
-		printf("\n \t %d \n",cnt);
+		//printf("\n \t %d \n",cnt);
 					strike_zeros = 0;
 				} else {
 					msg_ch[i] = '0';
-					printf("\n %c \n",msg_ch[i]);
+					//printf("\n %c \n",msg_ch[i]);
 					cnt++;
-		printf("\n \t %d \n",cnt);
+		//rintf("\n \t %d \n",cnt);
 					if (++strike_zeros >= 8 && i % 8 == 0) {
 						break;
 					}
@@ -49,9 +53,9 @@ bool detect_bit(uint64_t *addr);
 			// Print out message
 			int ascii_msg_len = binary_msg_len / 8;
 			char msg[ascii_msg_len];
-			printf(" %s\n", conv_char(msg_ch, ascii_msg_len, msg));
+			//printf(" %s\n", conv_char(msg_ch, ascii_msg_len, msg));
 			
-			printf("\n\n\n Accuracy (%%): %f\n", check_accuracy(msg_ch, ascii_msg_len)*100);
+			//printf("\n\n\n Accuracy (%%): %f\n", check_accuracy(msg_ch, ascii_msg_len)*100);
 	
 			
 		}

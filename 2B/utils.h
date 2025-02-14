0aa5ba73406e36455509ccf60609b853b63e9950
Kaon <78657124+KaonnoaK@users.noreply.github.com>
2025-02-14 17:05:02 +0530

    latest

--- 2B/utils.h
+++ 2B/utils.h
@@ -11,7 +11,7 @@
 #define MAX_MSG_SIZE 500
 
 #define CHANNEL_THRESHOLD 150
-#define MAX_ACCESSES 100
+#define MAX_ACCESSES 5
 
 #ifndef UTIL_H_
 #define UTIL_H_
@@ -20,32 +20,34 @@
 #define ADDR_PTR uint64_t
 #define CACHE_SIZE (8*1024*1024)
 #define CACHE_LINE_SIZE 64
+#define WAYS 16
+#define SETS 8192
 #define HALF_CACHE_SIZE (CACHE_SIZE/2)
-#define NUM_NODES (HALF_CACHE_SIZE / CACHE_LINE_SIZE )
+#define INTERVAL 0x00F00000
+#define NUM_NODES (HALF_CACHE_SIZE / CACHE_LINE_SIZE / 4)
 #define NUM_SETS (NUM_NODES / 16)
 
+
 #ifdef DUPLEX_MODE
-#define CHANNEL_SENDING_INTERVAL        0x00010000  // Increased interval for more cache filling time
-#define CHANNEL_SYNC_TIMEMASK           0x0001FFFF  // Increased time mask for lenient synchronization
+#define CHANNEL_SENDING_INTERVAL        0x00020000  // Increased interval for more cache filling time
+#define CHANNEL_SYNC_TIMEMASK           0x003FFFFF
 #else
 #define CHANNEL_SENDING_INTERVAL        0x00010000  // Increased interval for more cache filling time
-#define CHANNEL_SYNC_TIMEMASK           0x0001FFFF  // Increased time mask for lenient synchronization
+#define CHANNEL_SYNC_TIMEMASK           0x001FFFFF  // Increased time mask for lenient synchronization
 #endif
 
-#define CHANNEL_SYNC_JITTER             0x0200  // Increased jitter for better synchronization tolerance
+#define CHANNEL_SYNC_JITTER             0x0000003F  // Increased jitter for better synchronization tolerance
 
 #define MAX_BUFFER_LEN	1024
 
-
-
 struct config {
 	int interval;
-	uint8_t *addr;
+	uint64_t *addr;
 };
 
 typedef struct Node {
 	struct Node *next ;
-	uint8_t data[CACHE_LINE_SIZE - sizeof(struct Node *)];
+	uint64_t data;
 }Node;
 
 CYCLES mem_access(ADDR_PTR addr);
@@ -65,11 +67,11 @@ void init_configS(struct config *config, int argc, char **argv);
 
 void init_configR(struct config *config, int argc, char **argv);
 
-void fill_llc (uint8_t *base);
+void fill_llc (uint64_t *base);
 
 void pointer_chase(Node *head);
 
-Node *create_ll (void);
+void create_ll (Node**);
 
 #endif
 

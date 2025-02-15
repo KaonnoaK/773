97bc5725c1b6dd0656b8ed71cc56e995d97d2585
Kaon <78657124+KaonnoaK@users.noreply.github.com>
2025-02-15 12:14:49 +0530

    Update utils.c

--- 2A/utils.c
+++ 2A/utils.c
@@ -154,24 +154,4 @@ void init_config(struct config *config, int argc, char **argv)
 	*/
 }
 
-bool detect_bit(struct config *config)
-{
-		int hits = 0;
-		int misses = 0;
-
-	// Sync with sender
-	CYCLES start_t = cc_sync();
-	while ((get_time() - start_t) < config->interval) {
-		CYCLES access_time = mem_access(config->addr);
-        /*printf("%d\n", access_time);*/
-		// Check if detected 1 or 0
-		if (access_time > CHANNEL_THRESHOLD) {
-			misses++;
-		} else {
-			hits++;
-		}
-	}
-
-	return misses >= hits;
-}
 
